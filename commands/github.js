6961ed9485c82616cd675b79b69f986d22834209
heroestech68 <heroesytech6816@gmail.com>
2025-12-30 11:15:34 +0300

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -6,11 +6,11 @@ const path = require('path');
 
 async function githubCommand(sock, chatId, message) {
   try {
-    const res = await fetch('https://api.github.com/repos/heroestech68/BUGFIXED-SULEXH-XMD');
+    const res = await fetch('https://api.github.com/repos/heroestech68/BUGBOT');
     if (!res.ok) throw new Error('Error fetching repository data');
     const json = await res.json();
 
-    let txt = `*乂  BUGFIXED-SULEXH-XMD   乂*\n\n`;
+    let txt = `*乂  BUGBOT  乂*\n\n`;
     txt += `✩  *Name* : ${json.name}\n`;
     txt += `✩  *Watchers* : ${json.watchers_count}\n`;
     txt += `✩  *Size* : ${(json.size / 1024).toFixed(2)} MB\n`;
@@ -18,7 +18,7 @@ async function githubCommand(sock, chatId, message) {
     txt += `✩  *URL* : ${json.html_url}\n`;
     txt += `✩  *Forks* : ${json.forks_count}\n`;
     txt += `✩  *Stars* : ${json.stargazers_count}\n\n`;
-    txt += `💥 *BUGFIXED-SULEXH-XMD*`;
+    txt += `💥 *BUGBOT XMD*`;
 
     // Use the local asset image
     const imgPath = path.join(__dirname, '../assets/bot_image.jpg');
